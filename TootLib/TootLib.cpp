93d7467b0c0837c5a98834e9a12d7a79abeec50c
Jim Balcomb <jim.balcomb@gmail.com>
2025-07-21 13:18:58 -0400

    learning GitHub Actions

---
+++ TootLib/TootLib.cpp
@@ -0,0 +1,10 @@
+#include "TootLib.hpp"
+
+#include <iostream>
+
+void Hello_World()
+{
+
+    std::cout << "Hello, World" << std::endl;
+
+}
---
+++ TootLib/TootLib.hpp
@@ -0,0 +1,6 @@
+#ifndef TOOTLIB_HPP
+#define TOOTLIB_HPP
+
+void Hello_World(void);
+
+#endif  /* TOOTLIB_HPP */
