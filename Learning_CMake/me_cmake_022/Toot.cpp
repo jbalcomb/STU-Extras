01e6e2b42a6a4b168254b47e0e43966bf479bd77
Jim Balcomb <jim.balcomb@gmail.com>
2025-07-23 20:24:33 -0400

    me_cmake_022

---
+++ Learning_CMake/me_cmake_022/Toot.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+#include "Toot.hpp"
+
+#include "TootLib/TootLib.h"
+
+int main(int argc, char * argv[])
+{
+
+    std::cout << NUMBER << std::endl;
+
+    Hello_World();
+
+    return 0;
+
+}
---
+++ Learning_CMake/me_cmake_022/TootLib/TootLib.h
@@ -0,0 +1,18 @@
+#ifndef TOOTLIB_H
+#define TOOTLIB_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+void Hello_World(void);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* TOOTLIB_H */
